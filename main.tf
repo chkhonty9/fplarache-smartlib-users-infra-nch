4c776a3ff3195d0699bbdfcad37c63340f6b18e6
nouhaila <nohaila.chkhonty@gmail.com>
2024-11-24 20:00:23 +0100

    terraform

--- main.tf
+++ main.tf
@@ -28,7 +28,8 @@ resource "aws_ecs_task_definition" "app_task" {
     network_mode = "awsvpc"
     memory = "512"
     cpu = "256"
-    execution_role_arn = aws_iam_role.ecs_task_execution.arn
+    #execution_role_arn = aws_iam_role.ecs_task_execution.arn
+    execution_role_arn = "arn:aws:iam::your-account-id:role/ecsTaskExecutionRole"
 }
 
 resource "aws_ecs_service" "app_service" {
@@ -44,24 +45,24 @@ resource "aws_ecs_service" "app_service" {
   desired_count = 1
 }
 
-resource "aws_iam_role" "ecs_task_execution" {  
-  name = "ecsTaskExecutionRole"  
+# resource "aws_iam_role" "ecs_task_execution" {  
+#   name = "ecsTaskExecutionRole"  
 
-  assume_role_policy = jsonencode({  
-    Version = "2012-10-17"  
-    Statement = [  
-      {  
-        Effect = "Allow"  
-        Principal = {  
-          Service = "ecs-tasks.amazonaws.com"  
-        }  
-        Action = "sts:AssumeRole"  
-      }  
-    ]  
-  })  
-}  
+#   assume_role_policy = jsonencode({  
+#     Version = "2012-10-17"  
+#     Statement = [  
+#       {  
+#         Effect = "Allow"  
+#         Principal = {  
+#           Service = "ecs-tasks.amazonaws.com"  
+#         }  
+#         Action = "sts:AssumeRole"  
+#       }  
+#     ]  
+#   })  
+# }  
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {  
-  role       = aws_iam_role.ecs_task_execution.name  
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"  
-}
+# resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {  
+#   role       = aws_iam_role.ecs_task_execution.name  
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"  
+# }
