b14a59cd094e154f539cfe6f617493017134b6a9
nouhaila <nohaila.chkhonty@gmail.com>
2024-11-24 20:06:25 +0100

    terraform

--- variables.tf
+++ variables.tf
@@ -3,19 +3,19 @@ variable "aws_region" {
 }  
 
 variable "ecr_repository_name" {  
-  default = "dev-fplarache-smartlib-users-repo-nch"  
+  default = "dev-fplarache-smartlib-users-repo-NCh"  
 }  
 
 variable "ecs_cluster_name" {  
-  default = "dev-fplarache-smartlib-users-fgcluster-nch"  
+  default = "dev-fplarache-smartlib-users-fgcluster-NCh"  
 }  
 
 variable "ecs_task_family" {  
-  default = "dev-fplarache-smartlib-users-td-nch"  
+  default = "dev-fplarache-smartlib-users-td-NCh"  
 }  
 
 variable "ecs_service_name" {  
-  default = "dev-fplarache-smartlib-users-service-nch"  
+  default = "dev-fplarache-smartlib-users-service-NCh"  
 }  
 
 variable "subnet_ids" {  
